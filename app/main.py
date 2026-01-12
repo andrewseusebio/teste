eb90f1f4a6eaca6da5f409f1fcac002975941060
andrewseusebio <eusebiofcp@outlook.com>
2026-01-12 04:25:08 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 import asyncio
 from fastapi import FastAPI
-from bot import bot, dp
-from payments.webhook import router as webhook_router
-from handlers import start, loja, compra, saldo, admin, estoque, fila, suporte
+
+from app.bot import bot, dp
+from app.payments.webhook import router as webhook_router
+
+from app.handlers import (
+    start,
+    loja,
+    compra,
+    saldo,
+    admin,
+    estoque,
+    fila,
+    suporte
+)
 
 app = FastAPI()
 app.include_router(webhook_router)
+
 
 @app.on_event("startup")
 async def startup():
